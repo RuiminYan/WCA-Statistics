8c57ff4ff0eaaaebdfe7644166a27ac089ccf3d5
CubeRoot <46429560+RuiminYan@users.noreply.github.com>
2024-05-23 00:01:57 +0800

    Update NWR.sql

--- NWR.sql
+++ NWR.sql
@@ -1,11 +1,10 @@
-新人WR
-新人单次WR
+/* 新人单次WR
 ChatGPT提示词
-我在用MySQL Workbench,
-有表格competitions，列名有：id, year, month, day. 其中year, month, day表示比赛的年月日。
+用MySQL Workbench, 有表格competitions，列名有：id, year, month, day. 其中year, month, day表示比赛的年月日。
 有表格results，列名有：competitionId, eventId, personName, personId, best. 其中year, month, day表示比赛的年月日。
-现在，我希望eventId固定为skewb, best都只取正数，对于每一个personId, 在competition日期最早的competition (注意日期如何比较大小) 下，取所有 best的最小值，记为firstCompSingle。
+希望eventId固定为skewb, best都只取正数，对于每一个personId, 在competition日期最早的competition (注意日期如何比较大小) 下，取所有 best的最小值，记为firstCompSingle。
 输出的列有personName，personId，firstCompSingle, 按照firstCompSingle从小到大排序
+*/
 
 -- 创建一个派生表来存储每个personId最早的比赛日期
 WITH first_competition_dates AS (
@@ -40,7 +39,10 @@ ORDER BY
     firstCompSingle
 LIMIT 10;
 
-新人平均WR
+
+
+
+-- 新人平均WR
 -- 创建一个派生表来存储每个personId最早的比赛日期
 WITH first_competition_dates AS (
     SELECT 
