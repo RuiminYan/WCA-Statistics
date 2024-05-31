758dbc680ff0f72dbd1bfb4052f13e1538d808ac
CubeRoot <46429560+RuiminYan@users.noreply.github.com>
2024-05-31 11:16:05 +0800

    Update Ao4R.sql

--- Ao4R.sql
+++ Ao4R.sql
@@ -8,7 +8,7 @@ ChatGPT提示词
 
 有表格results，列名有：competitionId, eventId, roundTypeId, pos, best, average, personName, personId, personCountryId, formatId, value1, value2, value3, value4, value5, regionSingleRecord, regionAverageRecord. 
 
-设置eventId为333, personId为2019WANY36, 在相同competition下, 排除掉仅有小于等于3个average的情况, 仅考虑有4个average的情况, 按照roundTypeId的以下顺序排序: 1 (或d), 2  (或e), 3  (或g), f, 将这4个average分别记为R1, R2, R3, Fi. 当Fi小于等于0时，记Ao4R为DNF; 否则, 记Ao4R为它们的平均数 (四舍五入到个位).
+设置eventId为333, personId为2019WANY36, 在相同competition下, 排除掉仅有小于等于3个average的情况, 仅考虑有4个average的情况, 按照roundTypeId的以下顺序排序: 1 (或d), 2  (或e), 3  (或g), f, 将这4个average分别记为R1, R2, R3, Fi. 当Fi小于等于0时，记Ao4R为-1; 否则, 记Ao4R为它们的平均数 (四舍五入到个位).
 
 输出列有personName, personId, Ao4R, R1, R2, R3, Fi, name, 按照Ao4R从小到大排序
 
@@ -39,7 +39,7 @@ Ao4R AS (
         personName,
         personId,
         CASE
-            WHEN Fi <= 0 THEN 'DNF'
+            WHEN Fi <= 0 THEN -1
             ELSE ROUND((R1 + R2 + R3 + Fi) / 4)
         END AS Ao4R,
         R1,
