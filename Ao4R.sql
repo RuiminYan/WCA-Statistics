a4092c9df6c8c8450fe84de94e9852c4fbe04009
CubeRoot <46429560+RuiminYan@users.noreply.github.com>
2024-05-31 13:20:43 +0800

    Update Ao4R.sql

--- Ao4R.sql
+++ Ao4R.sql
@@ -10,9 +10,11 @@ ChatGPT提示词
 
 设置eventId为333, personId为2019WANY36, 在相同competition下, 排除掉仅有小于等于3个average的情况, 仅考虑有4个average的情况, 按照roundTypeId的以下顺序排序: 1 (或d), 2  (或e), 3  (或g), f, 将这4个average分别记为R1, R2, R3, Fi. 当Fi小于等于0时，记Ao4R为-1; 否则, 记Ao4R为它们的平均数 (四舍五入到个位).
 
-输出列有personName, personId, Ao4R, R1, R2, R3, Fi, name, 按照Ao4R从小到大排序. 注意: 需要排除掉Ao4R为Null的数据.
+输出列有personName, personId, Ao4R, R1, R2, R3, Fi, name, 按照Ao4R从小到大排序. 注意: 需要排除掉Ao4R为Null的数据. 给每个条目标上序号.
 
 */
+
+SET @row_number = 0;
 
 WITH temp AS (
     SELECT
@@ -63,6 +65,6 @@ FROM
 JOIN
     competitions c ON Ao4R.competitionId = c.id
 WHERE
-    Ao4R.Ao4R  > 0
+    Ao4R.Ao4R > 0
 ORDER BY
     Ao4R.Ao4R;
