2540f81e12a9ef04b20beae5c33a056455849ed1
CubeRoot <46429560+RuiminYan@users.noreply.github.com>
2024-06-01 13:19:48 +0800

    Update Mo5.sql

--- Mo5.sql
+++ Mo5.sql
@@ -1,1 +1,10 @@
-
+SELECT
+  CASE 
+    WHEN value1 <= 0 OR value2 <= 0 OR value3 <= 0 OR value4 <= 0 OR value5 <= 0 THEN -1
+    ELSE ROUND((value1 + value2 + value3 + value4 + value5) / 5)
+  END AS Mo5,
+  value1, value2, value3, value4, value5
+FROM
+  Results
+WHERE
+  personId = '2019WANY36' AND eventId = '333';
