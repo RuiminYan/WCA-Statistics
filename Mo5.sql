f20c6e1b0a4e54c1e24946602da4206a2e1816a3
CubeRoot <46429560+RuiminYan@users.noreply.github.com>
2024-06-01 13:21:09 +0800

    Update Mo5.sql

--- Mo5.sql
+++ Mo5.sql
@@ -1,10 +1,22 @@
 SELECT
+  r.personName,
+  r.personId,
   CASE 
-    WHEN value1 <= 0 OR value2 <= 0 OR value3 <= 0 OR value4 <= 0 OR value5 <= 0 THEN -1
-    ELSE ROUND((value1 + value2 + value3 + value4 + value5) / 5)
+    WHEN r.value1 <= 0 OR r.value2 <= 0 OR r.value3 <= 0 OR r.value4 <= 0 OR r.value5 <= 0 THEN -1
+    ELSE ROUND((r.value1 + r.value2 + r.value3 + r.value4 + r.value5) / 5)
   END AS Mo5,
-  value1, value2, value3, value4, value5
+  r.value1, r.value2, r.value3, r.value4, r.value5,
+  c.year,
+  c.month,
+  c.day,
+  c.name
 FROM
-  Results
+  Results r
+JOIN
+  Competitions c ON r.competitionId = c.id
 WHERE
-  personId = '2019WANY36' AND eventId = '333';
+  r.eventId = '333'
+HAVING
+  Mo5 > 0
+ORDER BY
+  Mo5;
