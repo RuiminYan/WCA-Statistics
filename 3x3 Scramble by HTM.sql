52361b294ef17dcd7cdc8769c77b42980d610808
CubeRoot <46429560+RuiminYan@users.noreply.github.com>
2024-06-03 15:21:45 +0800

    Update 3x3 Scramble by HTM.sql

--- 3x3 Scramble by HTM.sql
+++ 3x3 Scramble by HTM.sql
@@ -1,3 +1,3 @@
 select length(scramble)-length(replace(scramble,' ',''))+1 HTM, scramble, eventId, competitionId, scrambleNum, isExtra from Scrambles
-where eventId in ('333', '333oh', '333ft')
+where eventId in ('333', '333oh', '333ft', '333fm')
 order by  HTM -- DESC
