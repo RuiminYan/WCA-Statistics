9bca5dd5f37e031b8d67417f13f535d5d76f9726
CubeRoot <46429560+RuiminYan@users.noreply.github.com>
2024-06-05 20:03:51 +0800

    Update Ranking.sql

--- Ranking.sql
+++ Ranking.sql
@@ -2,11 +2,13 @@
 SELECT * 
 FROM Results 
 WHERE eventId = "333" AND best > 0
-ORDER BY best;
+ORDER BY best
+LIMIT 100;
 
 -- Persons
 SELECT personName, MIN(best) AS best 
 FROM Results 
 WHERE eventId = '333' AND best > 0 
 GROUP BY personName 
-ORDER BY best;
+ORDER BY best
+LIMIT 100;
