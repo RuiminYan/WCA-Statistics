fbbd3e0071c00d5ff503c2dc845e13cc4092b30b
CubeRoot <46429560+RuiminYan@users.noreply.github.com>
2024-06-09 01:37:25 +0800

    Update basic stat.sql

--- basic stat.sql
+++ basic stat.sql
@@ -1,3 +1,13 @@
+/*
+当5个value中至少有一个≤0时，variance取NULL;
+当value1~4至少有1个≤0时，wpa取NULL；
+当value1~4至少有2个≤0时，bpa取NULL.
+当计算出的median≤0时，median取NULL; worst同理.
+
+ORDER BY
+  worst; 的时候，剔除掉worst为NULL的
+*/
+
 SELECT
   r.personName,
   r.personId,
@@ -7,6 +17,7 @@ SELECT
   r.value3, 
   r.value4, 
   r.value5,
+  r.best,
   r.average,
   r.regionalSingleRecord,
   r.regionalAverageRecord,
@@ -19,8 +30,7 @@ SELECT
     WHEN r.value1 <= 0 OR r.value2 <= 0 OR r.value3 <= 0 OR r.value4 <= 0 OR r.value5 <= 0 THEN NULL
     ELSE ROUND((POW(r.value1 - r.average, 2) + POW(r.value2 - r.average, 2) + POW(r.value3 - r.average, 2) + POW(r.value4 - r.average, 2) + POW(r.value5 - r.average, 2)) / 5, 2)
   END AS variance,
-  -- Calculate worst, best
-  CASE WHEN LEAST(r.value1, r.value2, r.value3, r.value4, r.value5) <= 0 THEN NULL ELSE LEAST(r.value1, r.value2, r.value3, r.value4, r.value5) END AS best,
+  -- Calculate worst
   CASE WHEN GREATEST(r.value1, r.value2, r.value3, r.value4, r.value5) <= 0 THEN NULL ELSE GREATEST(r.value1, r.value2, r.value3, r.value4, r.value5) END AS worst,
   -- Calculate median
   (SELECT ROUND(AVG(val), 2) 
@@ -47,6 +57,7 @@ FROM (
     value3,
     value4,
     value5,
+	best,
     average,
     regionalSingleRecord,
     regionalAverageRecord,
@@ -61,4 +72,8 @@ JOIN
 ORDER BY
   worst IS NULL, worst;
 
- -- 按日期排 c.year, c.month, c.day;
+/*
+WHERE best > 0
+ORDER BY
+  best;
+*/
