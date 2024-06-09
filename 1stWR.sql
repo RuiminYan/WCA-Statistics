a8692608f5a0cc0a97757d53bd6ebce5a5a4eb21
CubeRoot <46429560+RuiminYan@users.noreply.github.com>
2024-06-10 01:27:33 +0800

    Update 1stWR.sql

--- 1stWR.sql
+++ 1stWR.sql
@@ -1,4 +1,5 @@
 -- 1stWR Single
+-- 1stWR Single
 WITH FirstComp AS (
     SELECT 
         r.personId,
@@ -12,7 +13,7 @@ WITH FirstComp AS (
     GROUP BY 
         r.personId
 ),
-FirstRound AS (
+FirstRound1 AS (
     SELECT
         r.personId,
         r.personName,
@@ -29,8 +30,26 @@ FirstRound AS (
         r.eventId = '333'
         AND r.roundTypeId IN ('1', '0', 'd')
         AND r.value1 > 0
+),
+FirstRound2 AS (
+    SELECT
+        r.personId,
+        r.personName,
+        r.personCountryId,
+        r.value1 AS firstSingle,
+        r.regionalSingleRecord,
+        STR_TO_DATE(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d') AS event_date,
+        c.name
+    FROM 
+        Results r
+    JOIN 
+        Competitions c ON r.competitionId = c.id
+    WHERE 
+        r.eventId = '333'
+        AND r.roundTypeId IN ('f', 'b', 'c')
+        AND r.value1 > 0
 )
--- 连接上述两个子查询，获取每个人首次参加比赛的第一轮第一把成绩
+
 SELECT
     fr.personName,
     fr.personId,
@@ -42,12 +61,20 @@ SELECT
 FROM
     FirstComp fc
 JOIN
-    FirstRound fr ON fc.personId = fr.personId AND fc.earliest_date = fr.event_date
+    (
+        SELECT * FROM FirstRound1
+        UNION ALL
+        SELECT * FROM FirstRound2
+        WHERE personId NOT IN (SELECT personId FROM FirstRound1)
+    ) fr ON fc.personId = fr.personId AND fc.earliest_date = fr.event_date
 ORDER BY
-    fr.first_single;
-
+    fr.firstSingle;
 
 -- 按日期排 fc.earliest_date;
+
+
+
+
 
 
 
@@ -69,7 +96,7 @@ WITH FirstComp AS (
     GROUP BY 
         r.personId
 ),
-FirstRound AS (
+FirstRound1 AS (
     SELECT
         r.personId,
         r.personName,
@@ -86,8 +113,26 @@ FirstRound AS (
         r.eventId = '333'
         AND r.roundTypeId IN ('1', '0', 'd')
         AND r.average > 0
+),
+FirstRound2 AS (
+    SELECT
+        r.personId,
+        r.personName,
+        r.personCountryId,
+        r.average AS firstAvg,
+        r.regionalAverageRecord,
+        STR_TO_DATE(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d') AS event_date,
+        c.name
+    FROM 
+        Results r
+    JOIN 
+        Competitions c ON r.competitionId = c.id
+    WHERE 
+        r.eventId = '333'
+        AND r.roundTypeId IN ('f', 'b', 'c')
+        AND r.average > 0
 )
--- 连接上述两个子查询，获取每个人首次参加比赛的第一轮平均成绩
+
 SELECT
     fr.personName,
     fr.personId,
@@ -99,11 +144,21 @@ SELECT
 FROM
     FirstComp fc
 JOIN
-    FirstRound fr ON fc.personId = fr.personId AND fc.earliest_date = fr.event_date
+    (
+        SELECT * FROM FirstRound1
+        UNION ALL
+        SELECT * FROM FirstRound2
+        WHERE personId NOT IN (SELECT personId FROM FirstRound1)
+    ) fr ON fc.personId = fr.personId AND fc.earliest_date = fr.event_date
 ORDER BY
     fr.firstAvg;
 
 -- 按日期排 fc.earliest_date;
+
+
+
+
+
 
 
 
@@ -126,7 +181,7 @@ WITH first_competition_dates AS (
     GROUP BY 
         r.personId
 ),
-values_with_parts AS (
+values_with_parts1 AS (
     SELECT
         r.personName,
         r.personId,
@@ -149,7 +204,32 @@ values_with_parts AS (
     JOIN
         first_competition_dates fcd ON r.personId = fcd.personId AND STR_TO_DATE(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d') = fcd.earliest_date
     WHERE
-        r.eventId = '333mbf' AND r.value1 > 0 AND r.value2 > 0 AND r.value3 > 0
+        r.eventId = '333mbf' AND r.roundTypeId IN ('1', '0', 'd') AND r.value1 > 0 AND r.value2 > 0 AND r.value3 > 0
+),
+values_with_parts2 AS (
+    SELECT
+        r.personName,
+        r.personId,
+        r.personCountryId,
+        c.year,
+        c.month,
+        c.day,
+        c.name,
+        r.value1,
+        r.value2,
+        r.value3,
+        r.regionalAverageRecord,
+        SUBSTRING(r.value1, 1, 2) AS dd1, SUBSTRING(r.value1, 3, 5) AS ttttt1, SUBSTRING(r.value1, 8, 2) AS mm1,
+        SUBSTRING(r.value2, 1, 2) AS dd2, SUBSTRING(r.value2, 3, 5) AS ttttt2, SUBSTRING(r.value2, 8, 2) AS mm2,
+        SUBSTRING(r.value3, 1, 2) AS dd3, SUBSTRING(r.value3, 3, 5) AS ttttt3, SUBSTRING(r.value3, 8, 2) AS mm3
+    FROM
+        results r
+    JOIN
+        competitions c ON r.competitionId = c.id
+    JOIN
+        first_competition_dates fcd ON r.personId = fcd.personId AND STR_TO_DATE(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d') = fcd.earliest_date
+    WHERE
+        r.eventId = '333mbf' AND r.roundTypeId IN ('f', 'b', 'c') AND r.value1 > 0 AND r.value2 > 0 AND r.value3 > 0
 ),
 average_values AS (
     SELECT
@@ -166,7 +246,11 @@ average_values AS (
         ROUND((CAST(ttttt1 AS UNSIGNED) + CAST(ttttt2 AS UNSIGNED) + CAST(ttttt3 AS UNSIGNED)) / 3) AS avg_ttttt,
         ROUND((CAST(mm1 AS UNSIGNED) + CAST(mm2 AS UNSIGNED) + CAST(mm3 AS UNSIGNED)) / 3) AS avg_mm
     FROM
-        values_with_parts
+        (SELECT * FROM values_with_parts1
+         UNION ALL
+         SELECT * FROM values_with_parts2
+         WHERE personId NOT IN (SELECT personId FROM values_with_parts1)
+        ) AS combined_values
 )
 SELECT
     personName,
@@ -183,3 +267,4 @@ FROM
     average_values
 ORDER BY
     date;
+
