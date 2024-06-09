f11b75e701d118820055f93c0d922691af892e33
CubeRoot <46429560+RuiminYan@users.noreply.github.com>
2024-06-09 16:34:24 +0800

    Create Duration.sql

---
+++ Duration.sql
@@ -0,0 +1,8 @@
+SELECT id AS competitionId,
+       CASE
+           WHEN DATEDIFF(CONCAT(year, '-', LPAD(endMonth, 2, '00'), '-', LPAD(endDay, 2, '00')), CONCAT(year, '-', LPAD(month, 2, '00'), '-', LPAD(day, 2, '00'))) + 1 <= 0
+               THEN DATEDIFF(CONCAT(year + 1, '-', LPAD(endMonth, 2, '00'), '-', LPAD(endDay, 2, '00')), CONCAT(year, '-', LPAD(month, 2, '00'), '-', LPAD(day, 2, '00'))) + 1
+           ELSE DATEDIFF(CONCAT(year, '-', LPAD(endMonth, 2, '00'), '-', LPAD(endDay, 2, '00')), CONCAT(year, '-', LPAD(month, 2, '00'), '-', LPAD(day, 2, '00'))) + 1
+       END AS duration
+FROM competitions
+ORDER BY duration DESC;
