80f841780789b8c90af2eb37cdb30ad57b26a044
CubeRoot <46429560+RuiminYan@users.noreply.github.com>
2024-06-09 12:13:44 +0800

    Update basic stat.sql

--- basic stat.sql
+++ basic stat.sql
@@ -1,7 +1,6 @@
 /*
--- 当value1~4恰好有1个≤0时，bpa取value1~4排除掉这个≤0的值后剩下的3个数的平均值???????????
-当计算出的median≤0时，median取NULL;??????????????????????????????
-当计算出的median≤0时，worst取NULL?????????????????????????????
+当计算出的median≤0时，median取NULL;
+当计算出的median≤0时，worst取NULL
 
 ORDER BY
   worst; 的时候，剔除掉worst为NULL的
@@ -32,14 +31,26 @@ SELECT
   -- Calculate worst
   CASE WHEN GREATEST(r.value1, r.value2, r.value3, r.value4, r.value5) <= 0 THEN NULL ELSE GREATEST(r.value1, r.value2, r.value3, r.value4, r.value5) END AS worst,
   -- Calculate median
-  (SELECT ROUND(AVG(val), 2) 
-   FROM (SELECT val 
-         FROM (SELECT r.value1 AS val UNION ALL SELECT r.value2 UNION ALL SELECT r.value3 UNION ALL SELECT r.value4 UNION ALL SELECT r.value5) sub 
-         ORDER BY val 
-         LIMIT 3, 1) median) AS median,
+  CASE 
+    WHEN (SELECT ROUND(AVG(val), 2) 
+          FROM (SELECT val 
+                FROM (SELECT r.value1 AS val UNION ALL SELECT r.value2 UNION ALL SELECT r.value3 UNION ALL SELECT r.value4 UNION ALL SELECT r.value5) sub 
+                ORDER BY val 
+                LIMIT 3, 1) median) <= 0 THEN NULL -- 当计算出的median≤0时，median取NULL
+
+    ELSE (SELECT ROUND(AVG(val), 2) 
+          FROM (SELECT val 
+                FROM (SELECT r.value1 AS val UNION ALL SELECT r.value2 UNION ALL SELECT r.value3 UNION ALL SELECT r.value4 UNION ALL SELECT r.value5) sub 
+                ORDER BY val 
+                LIMIT 3, 1) median)
+  END AS median,
   -- Calculate bpa and wpa
   CASE -- 当value1~4至少有2个≤0时，bpa取NULL
     WHEN (r.value1 <= 0 AND r.value2 <= 0) OR (r.value1 <= 0 AND r.value3 <= 0) OR (r.value1 <= 0 AND r.value4 <= 0) OR (r.value2 <= 0 AND r.value3 <= 0) OR (r.value2 <= 0 AND r.value4 <= 0) OR (r.value3 <= 0 AND r.value4 <= 0) THEN NULL
+    WHEN r.value1 <= 0 THEN ROUND((r.value2 + r.value3 + r.value4) / 3, 0) -- 当value1~4恰好有1个≤0时，bpa取value1~4排除掉这个≤0的值后剩下的3个数的平均值
+    WHEN r.value2 <= 0 THEN ROUND((r.value1 + r.value3 + r.value4) / 3, 0)
+    WHEN r.value3 <= 0 THEN ROUND((r.value1 + r.value2 + r.value4) / 3, 0)
+    WHEN r.value4 <= 0 THEN ROUND((r.value1 + r.value2 + r.value3) / 3, 0)
     ELSE ROUND((r.value1 + r.value2 + r.value3 + r.value4 - GREATEST(r.value1, r.value2, r.value3, r.value4)) / 3, 0)
   END AS bpa,
   CASE -- 当value1~4至少有1个≤0时，wpa取NULL
@@ -56,7 +67,7 @@ FROM (
     value3,
     value4,
     value5,
-	best,
+    best,
     average,
     regionalSingleRecord,
     regionalAverageRecord,
@@ -69,7 +80,8 @@ FROM (
 JOIN
   Competitions c ON r.competitionId = c.id
 ORDER BY
-  wpa IS NULL, wpa;
+  bpa IS NULL, bpa;
+
 
 /*
 WHERE best > 0
