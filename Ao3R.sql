37a9a6d8952d7569760dbb3232545579de8d1603
CubeRoot <46429560+RuiminYan@users.noreply.github.com>
2024-06-12 03:17:36 +0800

    Update Ao3R.sql

--- Ao3R.sql
+++ Ao3R.sql
@@ -59,11 +59,12 @@ SELECT
     Ao3R.personId,
     Ao3R.personCountryId,
     Ao3R.Ao3R,
-    Ao3R.R1,
-    Ao3R.R2,
-    Ao3R.Fi,
+    NULL as nothing,
     STR_TO_DATE(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d') AS date,
     c.name
+    Ao3R.R1 AS value1,
+    Ao3R.R2 AS value2,
+    Ao3R.Fi AS value3
 FROM
     Ao3R
 JOIN
