a18d000d896ee9654b79329cb549d76bff0964ee
CubeRoot <46429560+RuiminYan@users.noreply.github.com>
2024-06-14 04:29:43 +0800

    Update best_counting.sql

--- best_counting.sql
+++ best_counting.sql
@@ -4,23 +4,6 @@ CREATE TEMPORARY TABLE FilteredResults AS
 WITH RankedResults AS (
     SELECT
         r.personName,
-        -- 计算 best_counting
-        CASE
-            WHEN r.value1 <= 0 AND r.value2 <= 0 THEN 
-                (SELECT MIN(val) FROM (SELECT r.value3 AS val UNION ALL SELECT r.value4 UNION ALL SELECT r.value5) sub)
-            WHEN r.value1 <= 0 AND r.value3 <= 0 THEN 
-                (SELECT MIN(val) FROM (SELECT r.value2 AS val UNION ALL SELECT r.value4 UNION ALL SELECT r.value5) sub)
-            WHEN r.value1 <= 0 AND r.value4 <= 0 THEN 
-                (SELECT MIN(val) FROM (SELECT r.value2 AS val UNION ALL SELECT r.value3 UNION ALL SELECT r.value5) sub)
-            WHEN r.value2 <= 0 AND r.value3 <= 0 THEN 
-                (SELECT MIN(val) FROM (SELECT r.value1 AS val UNION ALL SELECT r.value4 UNION ALL SELECT r.value5) sub)
-            WHEN r.value2 <= 0 AND r.value4 <= 0 THEN 
-                (SELECT MIN(val) FROM (SELECT r.value1 AS val UNION ALL SELECT r.value3 UNION ALL SELECT r.value5) sub)
-            WHEN r.value3 <= 0 AND r.value4 <= 0 THEN 
-                (SELECT MIN(val) FROM (SELECT r.value1 AS val UNION ALL SELECT r.value2 UNION ALL SELECT r.value5) sub)
-            ELSE 
-                (SELECT MIN(val) FROM (SELECT r.value1 AS val UNION ALL SELECT r.value2 UNION ALL SELECT r.value3 UNION ALL SELECT r.value4 UNION ALL SELECT r.value5 ORDER BY val LIMIT 1 OFFSET 1) sub)
-        END AS best_counting,
         r.value1,
         r.value2,
         r.value3,
@@ -31,46 +14,48 @@ WITH RankedResults AS (
         c.name,
         STR_TO_DATE(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d') AS date,
         r.regionalAverageRecord,
+        CASE
+            WHEN r.value1 <= 0 OR r.value2 <= 0 OR r.value3 <= 0 OR r.value4 <= 0 OR r.value5 <= 0 THEN
+                GREATEST(
+                    LEAST(IF(r.value1 <= 0, 9999999999, r.value1), IF(r.value2 <= 0, 9999999999, r.value2), IF(r.value3 <= 0, 9999999999, r.value3), IF(r.value4 <= 0, 9999999999, r.value4)),
+                    LEAST(IF(r.value1 <= 0, 9999999999, r.value1), IF(r.value2 <= 0, 9999999999, r.value2), IF(r.value3 <= 0, 9999999999, r.value3), IF(r.value5 <= 0, 9999999999, r.value5)),
+                    LEAST(IF(r.value1 <= 0, 9999999999, r.value1), IF(r.value2 <= 0, 9999999999, r.value2), IF(r.value4 <= 0, 9999999999, r.value4), IF(r.value5 <= 0, 9999999999, r.value5)),
+                    LEAST(IF(r.value1 <= 0, 9999999999, r.value1), IF(r.value3 <= 0, 9999999999, r.value3), IF(r.value4 <= 0, 9999999999, r.value4), IF(r.value5 <= 0, 9999999999, r.value5)),
+                    LEAST(IF(r.value2 <= 0, 9999999999, r.value2), IF(r.value3 <= 0, 9999999999, r.value3), IF(r.value4 <= 0, 9999999999, r.value4), IF(r.value5 <= 0, 9999999999, r.value5))
+                )
+            ELSE GREATEST(
+                LEAST(r.value1, r.value2, r.value3, r.value4),
+                LEAST(r.value1, r.value2, r.value3, r.value5),
+                LEAST(r.value1, r.value2, r.value4, r.value5),
+                LEAST(r.value1, r.value3, r.value4, r.value5),
+                LEAST(r.value2, r.value3, r.value4, r.value5)
+            )
+        END AS best_counting,
         ROW_NUMBER() OVER (PARTITION BY STR_TO_DATE(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d') ORDER BY 
-            CASE
-                WHEN r.value1 <= 0 AND r.value2 <= 0 THEN 
-                    (SELECT MIN(val) FROM (SELECT r.value3 AS val UNION ALL SELECT r.value4 UNION ALL SELECT r.value5) sub)
-                WHEN r.value1 <= 0 AND r.value3 <= 0 THEN 
-                    (SELECT MIN(val) FROM (SELECT r.value2 AS val UNION ALL SELECT r.value4 UNION ALL SELECT r.value5) sub)
-                WHEN r.value1 <= 0 AND r.value4 <= 0 THEN 
-                    (SELECT MIN(val) FROM (SELECT r.value2 AS val UNION ALL SELECT r.value3 UNION ALL SELECT r.value5) sub)
-                WHEN r.value2 <= 0 AND r.value3 <= 0 THEN 
-                    (SELECT MIN(val) FROM (SELECT r.value1 AS val UNION ALL SELECT r.value4 UNION ALL SELECT r.value5) sub)
-                WHEN r.value2 <= 0 AND r.value4 <= 0 THEN 
-                    (SELECT MIN(val) FROM (SELECT r.value1 AS val UNION ALL SELECT r.value3 UNION ALL SELECT r.value5) sub)
-                WHEN r.value3 <= 0 AND r.value4 <= 0 THEN 
-                    (SELECT MIN(val) FROM (SELECT r.value1 AS val UNION ALL SELECT r.value2 UNION ALL SELECT r.value5) sub)
-                ELSE 
-                    (SELECT MIN(val) FROM (SELECT r.value1 AS val UNION ALL SELECT r.value2 UNION ALL SELECT r.value3 UNION ALL SELECT r.value4 UNION ALL SELECT r.value5 ORDER BY val LIMIT 1 OFFSET 1) sub)
-            END
+        CASE
+            WHEN r.value1 <= 0 OR r.value2 <= 0 OR r.value3 <= 0 OR r.value4 <= 0 OR r.value5 <= 0 THEN
+                GREATEST(
+                    LEAST(IF(r.value1 <= 0, 9999999999, r.value1), IF(r.value2 <= 0, 9999999999, r.value2), IF(r.value3 <= 0, 9999999999, r.value3), IF(r.value4 <= 0, 9999999999, r.value4)),
+                    LEAST(IF(r.value1 <= 0, 9999999999, r.value1), IF(r.value2 <= 0, 9999999999, r.value2), IF(r.value3 <= 0, 9999999999, r.value3), IF(r.value5 <= 0, 9999999999, r.value5)),
+                    LEAST(IF(r.value1 <= 0, 9999999999, r.value1), IF(r.value2 <= 0, 9999999999, r.value2), IF(r.value4 <= 0, 9999999999, r.value4), IF(r.value5 <= 0, 9999999999, r.value5)),
+                    LEAST(IF(r.value1 <= 0, 9999999999, r.value1), IF(r.value3 <= 0, 9999999999, r.value3), IF(r.value4 <= 0, 9999999999, r.value4), IF(r.value5 <= 0, 9999999999, r.value5)),
+                    LEAST(IF(r.value2 <= 0, 9999999999, r.value2), IF(r.value3 <= 0, 9999999999, r.value3), IF(r.value4 <= 0, 9999999999, r.value4), IF(r.value5 <= 0, 9999999999, r.value5))
+                )
+            ELSE GREATEST(
+                LEAST(r.value1, r.value2, r.value3, r.value4),
+                LEAST(r.value1, r.value2, r.value3, r.value5),
+                LEAST(r.value1, r.value2, r.value4, r.value5),
+                LEAST(r.value1, r.value3, r.value4, r.value5),
+                LEAST(r.value2, r.value3, r.value4, r.value5)
+            )
+        END
         ) AS rn
     FROM
         results r
     JOIN
         competitions c ON r.competitionId = c.id
     WHERE
-        r.eventId = '333' AND
-        CASE
-            WHEN r.value1 <= 0 AND r.value2 <= 0 THEN 
-                (SELECT MIN(val) FROM (SELECT r.value3 AS val UNION ALL SELECT r.value4 UNION ALL SELECT r.value5) sub)
-            WHEN r.value1 <= 0 AND r.value3 <= 0 THEN 
-                (SELECT MIN(val) FROM (SELECT r.value2 AS val UNION ALL SELECT r.value4 UNION ALL SELECT r.value5) sub)
-            WHEN r.value1 <= 0 AND r.value4 <= 0 THEN 
-                (SELECT MIN(val) FROM (SELECT r.value2 AS val UNION ALL SELECT r.value3 UNION ALL SELECT r.value5) sub)
-            WHEN r.value2 <= 0 AND r.value3 <= 0 THEN 
-                (SELECT MIN(val) FROM (SELECT r.value1 AS val UNION ALL SELECT r.value4 UNION ALL SELECT r.value5) sub)
-            WHEN r.value2 <= 0 AND r.value4 <= 0 THEN 
-                (SELECT MIN(val) FROM (SELECT r.value1 AS val UNION ALL SELECT r.value3 UNION ALL SELECT r.value5) sub)
-            WHEN r.value3 <= 0 AND r.value4 <= 0 THEN 
-                (SELECT MIN(val) FROM (SELECT r.value1 AS val UNION ALL SELECT r.value2 UNION ALL SELECT r.value5) sub)
-            ELSE 
-                (SELECT MIN(val) FROM (SELECT r.value1 AS val UNION ALL SELECT r.value2 UNION ALL SELECT r.value3 UNION ALL SELECT r.value4 UNION ALL SELECT r.value5 ORDER BY val LIMIT 1 OFFSET 1) sub)
-        END > 0
+        r.eventId = '333' AND r.best > 0
 )
 SELECT
     personName,
@@ -91,12 +76,12 @@ WHERE
     rn = 1;
 
 -- 第二步：使用变量逐步跟踪最小值
-SET @min_best_counting = 9999999999; -- 假设一个初始的最大值
+SET @min_best = 9999999999; -- 假设一个初始的最大值
 
 SELECT
     NULL AS flag,
     personName,
-    best_counting,
+    best_counting AS best,
     regionalAverageRecord,
     date,
     name,
@@ -121,13 +106,13 @@ FROM (
         value5,
         personId,
         personCountryId,
-        @min_best_counting := LEAST(@min_best_counting, best_counting) AS current_min_best_counting
+        @min_best := LEAST(@min_best, best_counting) AS current_min_best
     FROM
         FilteredResults
     ORDER BY
         date
 ) AS subquery
 WHERE
-    best_counting <= current_min_best_counting
+    best_counting <= current_min_best
 ORDER BY
     date;
