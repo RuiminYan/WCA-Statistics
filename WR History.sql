d787954ff8b858ff5706651e86129a958ab1f536
CubeRoot <46429560+RuiminYan@users.noreply.github.com>
2024-06-13 23:58:27 +0800

    Update WR History.sql

--- WR History.sql
+++ WR History.sql
@@ -1,6 +1,7 @@
--- 替换best为[目标函数]，确保所有涉及 best 的地方都使用新的[目标函数]计算逻辑
-
-
+/*
+替换best为[目标函数]，确保所有涉及 best 的地方都使用新的[目标函数]计算逻辑;
+将NULL AS record换成regionalSingleRecord或regionalAverageRecord.
+*/
 
 -- 第一步：删除并创建 FilteredResults 表
 DROP TEMPORARY TABLE IF EXISTS FilteredResults;
