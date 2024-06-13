fdf464eba14033dedd3c2215a25e787b12dcd0a6
CubeRoot <46429560+RuiminYan@users.noreply.github.com>
2024-06-13 14:13:53 +0800

    Update Single.sql

--- Single.sql
+++ Single.sql
@@ -1,44 +1,9 @@
--- 333ft (under mo3)
+-- 333ft
 SELECT
     r.personName,
     r.personId,
     r.personCountryId,
     -- 计算 value1, value2, value3 中最小的正数作为 single
-    LEAST(
-        CASE WHEN r.value1 > 0 THEN r.value1 ELSE 9999999999 END,
-        CASE WHEN r.value2 > 0 THEN r.value2 ELSE 9999999999 END,
-        CASE WHEN r.value3 > 0 THEN r.value3 ELSE 9999999999 END
-    ) AS single,
-    NULL AS nothing,
-    -- 将比赛日期转换为日期格式
-    STR_TO_DATE(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d') AS date,
-    c.name
-FROM
-    results r
-JOIN
-    competitions c ON r.competitionId = c.id
-WHERE
-    r.eventId = '333ft'
-    AND (r.value1 > 0 OR r.value2 > 0 OR r.value3 > 0)  -- 确保至少一个 value 大于0
-    AND r.value4 = 0 AND r.value5 = 0
-HAVING
-    single != 9999999999  -- 确保 single 不为极大值，排除无效记录
-ORDER BY
-    date;
-
-
-
-
-
-
-
-
--- 333ft (under ao5)
-SELECT
-    r.personName,
-    r.personId,
-    r.personCountryId,
-    -- 计算 value1, value2, value3, value4, value5 中最小的正数作为 single
     LEAST(
         CASE WHEN r.value1 > 0 THEN r.value1 ELSE 9999999999 END,
         CASE WHEN r.value2 > 0 THEN r.value2 ELSE 9999999999 END,
@@ -56,7 +21,7 @@ JOIN
     competitions c ON r.competitionId = c.id
 WHERE
     r.eventId = '333ft'
-    AND (r.value1 > 0 OR r.value2 > 0 OR r.value3 > 0 OR r.value4 > 0 OR r.value5 > 0)  -- 确保至少一个 value 大于0
+    AND (r.value1 > 0 OR r.value2 > 0 OR r.value3 > 0)  -- 确保至少一个 value 大于0
 HAVING
     single != 9999999999  -- 确保 single 不为极大值，排除无效记录
 ORDER BY
