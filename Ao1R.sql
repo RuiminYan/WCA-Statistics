8790f9b3ef09b37520f5dfa7c39525ff61544ba4
CubeRoot <46429560+RuiminYan@users.noreply.github.com>
2024-06-15 00:48:03 +0800

    Update Ao1R.sql

--- Ao1R.sql
+++ Ao1R.sql
@@ -1,4 +1,3 @@
--- 定义 selected_persons 表
 WITH selected_persons AS (
     SELECT DISTINCT personName, personId, personCountryId, average
     FROM (
@@ -7,7 +6,7 @@ WITH selected_persons AS (
         JOIN (
             SELECT personName, MIN(average) AS min_average
             FROM Results
-            WHERE eventId = '333' AND average > 0
+            WHERE eventId = '333bf' AND average > 0
             GROUP BY personName
         ) sub ON r.personName = sub.personName AND r.average = sub.min_average
         ORDER BY r.average
@@ -15,33 +14,29 @@ WITH selected_persons AS (
     ORDER BY average
     -- 可选 LIMIT 100
 ),
--- 定义 temp 表
 temp AS (
     SELECT
         r.competitionId,
         r.personName,
         r.personId,
         r.personCountryId,
-        r.regionalAverageRecord,
         MAX(CASE WHEN r.roundTypeId = 'f' THEN r.average END) AS Fi,
         COUNT(r.average) AS num_averages
     FROM
         results r
     WHERE
-        r.eventId = '333' AND r.personId IN (SELECT personId FROM selected_persons)
+        r.eventId = '333bf' AND r.personId IN (SELECT personId FROM selected_persons)
     GROUP BY
-        r.competitionId, r.personName, r.personId, r.personCountryId, r.regionalAverageRecord
+        r.competitionId, r.personName, r.personId, r.personCountryId
     HAVING
         num_averages = 1
 ),
--- 定义 Ao1R 表
 Ao1R AS (
     SELECT
         competitionId,
         personName,
         personId,
         personCountryId,
-        regionalAverageRecord,
         CASE
             WHEN Fi <= 0 THEN -1
             ELSE Fi
@@ -50,12 +45,11 @@ Ao1R AS (
     FROM
         temp
 )
--- 最终查询
 SELECT
     NULL AS flag,
     Ao1R.personName,
     Ao1R.Ao1R,
-    Ao1R.regionalAverageRecord,
+    NULL as nothing,
     STR_TO_DATE(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d') AS date,
     c.name,
     Ao1R.Fi AS value1,
