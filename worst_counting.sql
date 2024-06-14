9443cd27a97d2a84b4025d734bcd1a0a9848bf8e
CubeRoot <46429560+RuiminYan@users.noreply.github.com>
2024-06-14 12:12:01 +0800

    Update worst_counting.sql

--- worst_counting.sql
+++ worst_counting.sql
@@ -55,7 +55,24 @@ WITH RankedResults AS (
     JOIN
         competitions c ON r.competitionId = c.id
     WHERE
-        r.eventId = '333' AND r.best > 0
+        r.eventId = '333' AND 
+            CASE
+            WHEN r.value1 <= 0 OR r.value2 <= 0 OR r.value3 <= 0 OR r.value4 <= 0 OR r.value5 <= 0 THEN
+                LEAST(
+                    GREATEST(IF(r.value1 <= 0, 9999999999, r.value1), IF(r.value2 <= 0, 9999999999, r.value2), IF(r.value3 <= 0, 9999999999, r.value3), IF(r.value4 <= 0, 9999999999, r.value4)),
+                    GREATEST(IF(r.value1 <= 0, 9999999999, r.value1), IF(r.value2 <= 0, 9999999999, r.value2), IF(r.value3 <= 0, 9999999999, r.value3), IF(r.value5 <= 0, 9999999999, r.value5)),
+                    GREATEST(IF(r.value1 <= 0, 9999999999, r.value1), IF(r.value2 <= 0, 9999999999, r.value2), IF(r.value4 <= 0, 9999999999, r.value4), IF(r.value5 <= 0, 9999999999, r.value5)),
+                    GREATEST(IF(r.value1 <= 0, 9999999999, r.value1), IF(r.value3 <= 0, 9999999999, r.value3), IF(r.value4 <= 0, 9999999999, r.value4), IF(r.value5 <= 0, 9999999999, r.value5)),
+                    GREATEST(IF(r.value2 <= 0, 9999999999, r.value2), IF(r.value3 <= 0, 9999999999, r.value3), IF(r.value4 <= 0, 9999999999, r.value4), IF(r.value5 <= 0, 9999999999, r.value5))
+                )
+            ELSE LEAST(
+                GREATEST(r.value1, r.value2, r.value3, r.value4),
+                GREATEST(r.value1, r.value2, r.value3, r.value5),
+                GREATEST(r.value1, r.value2, r.value4, r.value5),
+                GREATEST(r.value1, r.value3, r.value4, r.value5),
+                GREATEST(r.value2, r.value3, r.value4, r.value5)
+            )
+        END > 0
 )
 SELECT
     personName,
@@ -76,12 +93,12 @@ WHERE
     rn = 1;
 
 -- 第二步：使用变量逐步跟踪最小值
-SET @min_best = 9999999999; -- 假设一个初始的最大值
+SET @min_worst_counting = 9999999999; -- 假设一个初始的最大值
 
 SELECT
     NULL AS flag,
     personName,
-    worst_counting AS best,
+    worst_counting,
     regionalAverageRecord,
     date,
     name,
@@ -106,13 +123,13 @@ FROM (
         value5,
         personId,
         personCountryId,
-        @min_best := LEAST(@min_best, worst_counting) AS current_min_best
+        @min_worst_counting := LEAST(@min_worst_counting, worst_counting) AS current_min_worst_counting
     FROM
         FilteredResults
     ORDER BY
         date
 ) AS subquery
 WHERE
-    worst_counting <= current_min_best
+    worst_counting <= current_min_worst_counting
 ORDER BY
     date;
