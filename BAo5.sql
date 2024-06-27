d4f0afde462d385d5748d78bf86d28f50477d220
CubeRoot <46429560+RuiminYan@users.noreply.github.com>
2024-06-27 18:34:27 +0800

    Update BAo5.sql

--- BAo5.sql
+++ BAo5.sql
@@ -1,11 +1,3 @@
-/*
-BAo5的方法如下：
-如果5个value有超过2个≤0，则BAo5取为NULL
-如果5个value有2个≤0，则BAo5取为剩下3个value的平均
-如果5个value有1个≤0，则BAo5取为剩下4个value的和减去最大value,再除以3
-如果5个value没有≤0，则BAo5取为5个value的和减去最大value, 再减去第二大value, 再除以3
-*/
-
 -- 第一步：删除并创建 FilteredResults 表
 DROP TEMPORARY TABLE IF EXISTS FilteredResults;
 CREATE TEMPORARY TABLE FilteredResults AS
