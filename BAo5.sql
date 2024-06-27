29b373443530b0b44cd114e1890684e65b8b58c1
CubeRoot <46429560+RuiminYan@users.noreply.github.com>
2024-06-27 17:43:37 +0800

    Update BAo5.sql

--- BAo5.sql
+++ BAo5.sql
@@ -3,5 +3,5 @@ BAo5的方法如下：
 如果5个value有超过2个≤0，则BAo5取为NULL
 如果5个value有2个≤0，则BAo5取为剩下3个value的平均
 如果5个value有1个≤0，则BAo5取为剩下4个value的和减去最大value,再除以3
-如果5个value没有≤0，则BAo5取为前三小的value的平均
+如果5个value没有≤0，则BAo5取为5个value的和减去最大value, 再减去第二大value, 再除以3
 */
