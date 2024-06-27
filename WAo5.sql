53ce74d22e274ccd789afc5d96105446c61c534f
CubeRoot <46429560+RuiminYan@users.noreply.github.com>
2024-06-27 19:17:46 +0800

    Create WAo5.sql

---
+++ WAo5.sql
@@ -0,0 +1,98 @@
+-- 第一步：删除并创建 FilteredResults 表
+DROP TEMPORARY TABLE IF EXISTS FilteredResults;
+CREATE TEMPORARY TABLE FilteredResults AS
+WITH RankedResults AS (
+    SELECT
+        r.personName,
+        CASE
+            WHEN r.value1 <= 0 OR r.value2 <= 0 OR r.value3 <= 0 OR r.value4 <= 0 OR r.value5 <= 0 THEN NULL
+            ELSE ROUND((r.value1 + r.value2 + r.value3 + r.value4 + r.value5 - 
+                        LEAST(r.value1, r.value2, r.value3, r.value4, r.value5) - 
+                        (SELECT val FROM (SELECT r.value1 AS val UNION ALL SELECT r.value2 UNION ALL SELECT r.value3 UNION ALL SELECT r.value4 UNION ALL SELECT r.value5 ORDER BY val LIMIT 1, 1) AS subquery)) / 3, 0)
+        END AS WAo5, -- 目标函数
+        r.value1,
+        r.value2,
+        r.value3,
+        r.value4,
+        r.value5,
+        r.personId,
+        r.personCountryId,
+        c.name,
+        STR_TO_DATE(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d') AS date,
+        r.regionalAverageRecord,
+        ROW_NUMBER() OVER (PARTITION BY STR_TO_DATE(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d') ORDER BY CASE
+            WHEN r.value1 <= 0 OR r.value2 <= 0 OR r.value3 <= 0 OR r.value4 <= 0 OR r.value5 <= 0 THEN NULL
+            ELSE ROUND((r.value1 + r.value2 + r.value3 + r.value4 + r.value5 - 
+                        LEAST(r.value1, r.value2, r.value3, r.value4, r.value5) - 
+                        (SELECT val FROM (SELECT r.value1 AS val UNION ALL SELECT r.value2 UNION ALL SELECT r.value3 UNION ALL SELECT r.value4 UNION ALL SELECT r.value5 ORDER BY val LIMIT 1, 1) AS subquery)) / 3, 0)
+        END) AS rn -- 使用目标函数
+    FROM
+        results r
+    JOIN
+        competitions c ON r.competitionId = c.id
+    WHERE
+        r.eventId = '333' AND CASE
+            WHEN r.value1 <= 0 OR r.value2 <= 0 OR r.value3 <= 0 OR r.value4 <= 0 OR r.value5 <= 0 THEN NULL
+            ELSE ROUND((r.value1 + r.value2 + r.value3 + r.value4 + r.value5 - 
+                        LEAST(r.value1, r.value2, r.value3, r.value4, r.value5) - 
+                        (SELECT val FROM (SELECT r.value1 AS val UNION ALL SELECT r.value2 UNION ALL SELECT r.value3 UNION ALL SELECT r.value4 UNION ALL SELECT r.value5 ORDER BY val LIMIT 1, 1) AS subquery)) / 3, 0)
+        END > 0 -- 使用目标函数
+)
+SELECT
+    personName,
+    WAo5, -- 使用目标函数名称
+    regionalAverageRecord,
+    date,
+    name,
+    value1,
+    value2,
+    value3,
+    value4,
+    value5,
+    personId,
+    personCountryId
+FROM
+    RankedResults
+WHERE
+    rn = 1; -- 如果同一个纪录在一天内多次被打破，只有最好的那个成绩被认为打破了该纪录; 这里有bug, 如果同一天有2把相同的WR, 则只会选择一个
+
+-- 第二步：使用变量逐步跟踪最小值
+SET @min_WAo5 = 9999999999; -- 假设一个初始的最大值 -- 使用目标函数名称
+
+SELECT
+    personName,
+    WAo5, -- 使用目标函数名称
+    regionalAverageRecord,
+    date,
+    name,
+    value1,
+    value2,
+    value3,
+    value4,
+    value5,
+    personId,
+    personCountryId
+FROM (
+    SELECT
+        personName,
+        WAo5, -- 使用目标函数名称
+        regionalAverageRecord,
+        date,
+        name,
+        value1,
+        value2,
+        value3,
+        value4,
+        value5,
+        personId,
+        personCountryId,
+        @min_WAo5 := LEAST(@min_WAo5, WAo5) AS current_min_WAo5 -- 使用目标函数名称
+    FROM
+        FilteredResults
+    ORDER BY
+        date
+) AS subquery
+WHERE
+    WAo5 <= current_min_WAo5 -- 使用目标函数名称
+ORDER BY
+    date;
