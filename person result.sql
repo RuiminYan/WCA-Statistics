41b5d93ccc16a9f85e1e4201e1599c11f1b33dd3
CubeRoot <46429560+RuiminYan@users.noreply.github.com>
2024-07-07 11:44:56 +0800

    Update person result.sql

--- person result.sql
+++ person result.sql
@@ -1,3 +1,17 @@
 SELECT * -- value1, value2, value3, value4, value5
 FROM Results
 WHERE personId = "2019WANY36" AND eventId = "333";
+
+
+
+-- 含date
+SELECT 
+    r.*,
+    STR_TO_DATE(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d') AS date
+FROM 
+    Results r
+JOIN 
+    Competitions c ON r.competitionId = c.id
+WHERE 
+    r.personId = "2019WANY36" 
+    AND r.eventId = "333";
