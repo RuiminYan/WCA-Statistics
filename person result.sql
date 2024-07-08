51298779ba387d4a43f382016cc4167ecab53fe4
CubeRoot <46429560+RuiminYan@users.noreply.github.com>
2024-07-09 03:21:42 +0800

    Update person result.sql

--- person result.sql
+++ person result.sql
@@ -17,3 +17,119 @@ WHERE
     AND r.eventId = '333'
 ORDER BY 
     date;
+
+
+
+
+
+
+
+
+
+
+
+/*
+单次成绩
+想用一列value输出所有value，先按日期排，然后按roundTypeId排，然后按value1,value2.value3,value4,value5排
+*/
+WITH AllValues AS (
+    SELECT 
+        r.personName,
+		r.value1 AS value,
+        STR_TO_DATE(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d') AS date,
+		c.name,
+        r.roundTypeId,
+        '1' AS value_order,
+		r.personId
+    FROM 
+        Results r
+    JOIN 
+        Competitions c ON r.competitionId = c.id
+    WHERE 
+        r.personId = '2019WANY36'
+        AND r.eventId = '333'
+        AND r.value1 IS NOT NULL
+
+    UNION ALL
+
+    SELECT 
+        r.personName,
+        r.value2 AS value,
+        STR_TO_DATE(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d') AS date,
+		c.name,
+        r.roundTypeId,
+        '2' AS value_order,
+		r.personId
+    FROM 
+        Results r
+    JOIN 
+        Competitions c ON r.competitionId = c.id
+    WHERE 
+        r.personId = '2019WANY36'
+        AND r.eventId = '333'
+        AND r.value2 IS NOT NULL
+
+    UNION ALL
+
+    SELECT 
+        r.personName,
+        r.value3 AS value,
+        STR_TO_DATE(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d') AS date,
+		c.name,
+        r.roundTypeId,
+        '3' AS value_order,
+		r.personId
+    FROM 
+        Results r
+    JOIN 
+        Competitions c ON r.competitionId = c.id
+    WHERE 
+        r.personId = '2019WANY36'
+        AND r.eventId = '333'
+        AND r.value3 IS NOT NULL
+
+    UNION ALL
+
+    SELECT 
+        r.personName,
+        r.value4 AS value,
+        STR_TO_DATE(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d') AS date,
+		c.name,
+        r.roundTypeId,
+        '4' AS value_order,
+		r.personId
+    FROM 
+        Results r
+    JOIN 
+        Competitions c ON r.competitionId = c.id
+    WHERE 
+        r.personId = '2019WANY36'
+        AND r.eventId = '333'
+        AND r.value4 IS NOT NULL
+
+    UNION ALL
+
+    SELECT 
+        r.personName,
+        r.value5 AS value,
+        STR_TO_DATE(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d') AS date,
+		c.name,
+        r.roundTypeId,
+        '5' AS value_order,
+		r.personId
+    FROM 
+        Results r
+    JOIN 
+        Competitions c ON r.competitionId = c.id
+    WHERE 
+        r.personId = '2019WANY36'
+        AND r.eventId = '333'
+        AND r.value5 IS NOT NULL
+)
+
+SELECT *
+FROM AllValues
+ORDER BY 
+    date,
+    FIELD(roundTypeId, '0', '1', 'd', '2', 'e', '3', 'g', 'f', 'b', 'c'),
+    value_order;
