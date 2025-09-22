6135748a85108f0b92d494297769cbba2fa2cad8
CubeRoot <46429560+RuiminYan@users.noreply.github.com>
2025-09-22 16:01:38 +0800

    Update 3x3 Avg TOP100.sql

--- 3x3 Avg TOP100.sql
+++ 3x3 Avg TOP100.sql
@@ -16,3 +16,27 @@ WHERE
 ORDER BY 
     r.average ASC
 LIMIT 100;
+
+
+
+
+
+***统计席位***
+SELECT COUNT(*) AS count_Yiheng
+FROM (
+    SELECT 
+        r.personName,
+        r.personId,
+        r.personCountryId,
+        r.average,
+        STR_TO_DATE(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d') AS comp_date,
+        c.name AS competition_name
+    FROM wca_export.Results r
+    JOIN wca_export.Competitions c ON r.competitionId = c.id
+    WHERE r.eventId = '333'
+      AND r.average > 0
+      AND STR_TO_DATE(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d') < '2024-08-25'
+    ORDER BY r.average ASC
+    LIMIT 100
+) t
+WHERE t.personName = 'Yiheng Wang (王艺衡)';
