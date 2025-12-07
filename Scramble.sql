6a2b74db7bafb11a25a808e37c28311bff4c5a6e
CubeRoot <46429560+RuiminYan@users.noreply.github.com>
2025-12-07 23:39:36 +0800

    Update and rename 3x3 Scramble by Movecount.sql to Scramble.sql

--- Scramble.sql
+++ Scramble.sql
@@ -1,3 +1,4 @@
+-- 3x3 Scramble by Movecount
 -- HTM
 select length(scramble)-length(replace(scramble,' ',''))+1 HTM, scramble, eventId, competitionId, scrambleNum, isExtra from Scrambles
 where eventId in ('333', '333oh', '333ft', '333fm')
@@ -18,3 +19,41 @@ WHERE
     eventId IN ('333', '333oh', '333ft', '333fm')
 ORDER BY
     QTM;  -- DESC
+
+
+
+-- 打乱按编号
+SELECT scramble
+FROM wca_export.scrambles
+WHERE eventId in ('333', '333oh', '333ft', '333fm')
+ORDER BY scrambleId
+LIMIT 10000, 10000;
+
+
+
+-- 打乱按编号，加入行号和其他信息
+SELECT 
+    @rownum := @rownum + 1 AS rn,
+    competitionId,
+    eventId,
+    isExtra,
+    scramble
+FROM wca_export.scrambles, (SELECT @rownum := 0) AS r
+WHERE eventId in ('333', '333oh', '333ft', '333fm')
+ORDER BY scrambleId;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
